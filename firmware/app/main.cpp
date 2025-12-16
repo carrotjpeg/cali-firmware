b132a611f4d9ed0df8c8eb350a50d30e220e12ba
carro._.t <evanathomson@gmail.com>
2025-12-16 10:40:36 -0800

    Remove UART logging in favor of BLE

--- firmware/app/main.cpp
+++ firmware/app/main.cpp
@@ -1,17 +1,11 @@
 #include "../drivers/common/i2c_bus.h"
-#include "../drivers/common/uart_log.h"
 #include "../drivers/tmp117/tmp117.h"
 
 int main() {
-    i2c_init();
-    uart_log_init();
+    i2c_init();\
 
     TMP117 temp;
     float temp_c = 0.0f;
 
-    while (1) {
-        if (temp.readTemperatureC(temp_c)) {
-            uart_log_printf("TMP117: %.2f C\n", temp_c);
-        }
-    }
+
 }
--- firmware/drivers/common/uart_log.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void uart_log_init();
-void uart_log_printf(const char* format, ...);
